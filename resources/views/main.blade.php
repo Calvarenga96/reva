f992000248d0d108e6cd36c7b455805493979c63
Christian Alvarenga <christianalvarenga201014@gmail.com>
2024-02-01 10:05:55 -0300

    project start

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Reva</title>
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.5/dist/cdn.min.js"></script>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+</body>
+
+</html>
